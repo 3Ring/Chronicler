d0a2fc160393918fad8f5fb9eac80ef6615f79d2
3Ring <celestialswashbuckler@gmail.com>
2021-06-10 16:35:03 -0400

    fix import of classes.py

--- BONapp.py
+++ BONapp.py
@@ -9,7 +9,7 @@ from wtforms.validators import DataRequired
 # from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 # from werkzeug.security import check_password_hash, generate_password_hash
 
-from utils.dbclasses import *
+from utils.classes import *
 from utils.BONhelpers import login_required
 
 # Configure application
