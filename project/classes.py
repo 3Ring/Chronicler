2e28135405c9df15bbd82d79e5a84058b6647836
3Ring <celestialswashbuckler@gmail.com>
2021-06-16 11:04:55 -0400

    changed password field on register to have a label of "password" not hash

--- project/classes.py
+++ project/classes.py
@@ -10,7 +10,7 @@ from . import db
 class UserForm(FlaskForm):
     username = StringField("Name", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired()])
-    hash = StringField("Hash", validators=[DataRequired ()])
+    hash = StringField("Password", validators=[DataRequired ()])
     realname = StringField("Real Name")
     submit = SubmitField("Submit")
 
