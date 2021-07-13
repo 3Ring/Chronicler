11dcde57b6bf8e13adc70feb6df344dd9901a79f
3Ring <celestialswashbuckler@gmail.com>
2021-07-13 10:24:53 -0400

    reorganized the settings out of create_app() and placed in their own file. cleaned up the procfile

--- project/__init__.py
+++ project/__init__.py
@@ -1,30 +1,10 @@
-from flask import Flask
 from flask_login import LoginManager
 from flask_migrate import Migrate
-from .settings import db
-import os
-
-
-def postfix(string):
-        if string[0:9] == 'postgres:':
-                new = 'postgresql' + string[8:]
-                return new
-        else:
-                return string
+from .settings import app, db
 
 def create_app():
-    app = Flask(__name__)
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-    db_password = os.environ.get('DB_PASS')
-    app.config['SECRET_KEY'] = db_password or 'so-secret-I-have-no-idea-what-it-is'
-    # PostGres config:
-    app.config['SQLALCHEMY_DATABASE_URI'] = postfix(os.environ.get('DATABASE_URL')) or 'mysql+pymysql://root:' + db_password + '@localhost/BON'
 
     migrate = Migrate(app, db)
-
-    # SQLite address
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///liteBON.db'
 
     from .classes import Users
 
--- project/settings.py
+++ project/settings.py
@@ -1,4 +1,22 @@
+import os
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-# Initialize the database
-db = SQLAlchemy()
+# this function is here because heroku returns "postgres://.." which is depreciated and not accepted by sqlalchemy. it should be 'postgresql://...'
+def postfix(string):
+    if string is None:
+        return None
+    else:
+        if string[0:9] == 'postgres:':
+            new = 'postgresql' + string[8:]
+            return new
+        else:
+            return string
+
+db_password = os.environ.get('DB_PASS')
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = db_password or 'so-secret-I-have-no-idea-what-it-is'
+app.config['SQLALCHEMY_DATABASE_URI'] = postfix(os.environ.get('DATABASE_URL')) or 'mysql+pymysql://root:' + db_password + '@localhost/BON'
+db = SQLAlchemy(app)
