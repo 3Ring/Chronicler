cc7f14a64f210dccf14292f1d1c1686f10d1921f
3Ring <celestialswashbuckler@gmail.com>
2021-08-22 12:32:24 -0400

    add duplicate checking for sessions

--- project/events.py
+++ project/events.py
@@ -2,8 +2,6 @@ from .__init__ import db, socketio
 from .classes import *
 from flask_socketio import emit
 from .helpers import validate as v
-def called():
-    print('\n\n\n\n', 'received', '\n\n\n\n')
 
 @socketio.on('send_new_session')
 def send_new_session(id, number, title, synopsis=None):
@@ -24,22 +22,15 @@ def send_new_note(user_id, game_id, note, private=False, in_character=False):
         private = True
     else:
         private = False
-    # current_game = Games.query.filter_by(id=game_id).first()
-
 
     current_char=Characters.query.filter_by(user_id=user_id, game_id=game_id).first()
     session_number=Sessions.query.with_entities(Sessions.number).filter_by(games_id=game_id).order_by(Sessions.number.desc()).first()[0]
     # this will cause issues if a player has more than one character for now
-
-    print('\n\n\n\n', "game_id:", game_id, "user_id:", user_id, "note:", note, "Private:", private, "in_character", in_character, "charname", current_char.name, "character(id):", current_char.id, "session_number:", session_number)
-
     new=Notes(charname=current_char.name, note=note, session_number=session_number, private=private, in_character=in_character, character=current_char.id , user_id=user_id, game_id=game_id)
     
     db.session.add(new)
     db.session.flush()
     new_note=str("<p id='note"+str(new.id)+"'>"+str(new.charname)+":  "+str(new.note)+"</p>")
-    print('\n\n\n\n', new_note)
-    
     emit('fill_new_note', (new_note, new.private, new.session_number, new.in_character), broadcast=True)
     db.session.commit()
 
