f1df199a4071191a76f0778498e6ca24207ae982
3Ring <celestialswashbuckler@gmail.com>
2021-09-12 16:39:03 -0400

    empty

---
+++ migrations/versions/1fb9402948cf.py
@@ -0,0 +1,24 @@
+"""test
+
+Revision ID: 48a7a4e7b6cc
+Revises: 47ebd263e92d
+Create Date: 2021-09-12 20:29:44.293904
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '48a7a4e7b6cc'
+down_revision = '47ebd263e92d'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
