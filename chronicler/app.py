56d5da4744bd42f8dfec8536f1bc25323559a278
3Ring <celestialswashbuckler@gmail.com>
2021-09-27 10:44:34 -0400

    still moving things around

---
+++ chronicler/app.py
@@ -0,0 +1,6 @@
+from project.api.__init__ import create_app
+
+app = create_app
+
+def test():
+    print("yes")
---
+++ chronicler/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages
+
+setup(name='chronicler', version='1.0', packages=find_packages())
