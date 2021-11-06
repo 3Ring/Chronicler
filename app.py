ba0eccf9f14fca263dd5549c1bbaf6dfaca1d0ed
3Ring <celestialswashbuckler@gmail.com>
2021-11-06 14:44:58 -0400

    heroku changes

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-# import os
+import os
 
 from project.__init__ import create_app
 
-# if os.environ.get("HEROKU_HOSTING"):
-#     app = create_app()
-app = create_app
+if os.environ.get("HEROKU_HOSTING"):
+    app = create_app()
+else:
+    app = create_app
