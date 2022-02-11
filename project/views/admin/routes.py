13f3354483e2fe7bde640f524c20b00171c6c25a
3Ring <celestialswashbuckler@gmail.com>
2022-02-11 11:32:37 -0500

    heroku

--- project/views/admin/routes.py
+++ project/views/admin/routes.py
@@ -3,6 +3,8 @@ from flask_admin import AdminIndexView, expose
 from flask import redirect, url_for, Blueprint, flash
 from flask_login import current_user
 
+
+
 admin_ = Blueprint("admin_", __name__)
 
 from project.setup_ import defaults as d
@@ -21,12 +23,25 @@ class AdminIndex(AdminIndexView):
 @admin_.route("/admin/engage", methods=["GET"])
 def engage():
     from project.helpers.db_session import db_session
-    from project.models import Users, Characters, Games
+    from project.models import Users, Characters, Games, Notes
+    from project.models import BridgeGameCharacters
     with db_session():
         
         for user in Users.query.all():
-            avatar = Characters.create(name=user.name, user_id=user.id, avatar=True)
-            avatar.add_to_game(Games.get_bugs().id)
-            flash(avatar)
+            # for c in Characters.query.filter_by(user_id=user.id).all():
+            #     if c.avatar == True:
+            #         for n in Notes.query.filter_by(origin_character_id=c.id).all():
+            #             flash(n.origin_character_id)
+            #             n.delete_self()
+            #         for br in BridgeGameCharacters.query.filter_by(character_id=c.id).all():
+
+            #             flash(br.game_id)
+            #             br.delete_self()
+            #         flash(c.name)
+            #         c.delete_self()
+            if user.id > 0:
+                avatar = Characters.create(name=user.name, user_id=user.id, avatar=True)
+                avatar.add_to_game(Games.get_bugs().id)
+                flash(avatar.name)
 
         return redirect(url_for("admin.index"))
