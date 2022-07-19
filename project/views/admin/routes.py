2ea4e66cf6d7930ffdad7c5e31a4ffcf9d9e512b
3Ring <celestialswashbuckler@gmail.com>
2022-07-19 15:18:33 -0400

    changing Luke's pw

--- project/views/admin/routes.py
+++ project/views/admin/routes.py
@@ -20,13 +20,22 @@ class AdminIndex(AdminIndexView):
         
         return self.render('admin/index.html', var=var)
     
-@admin_.route("/admin/engage", methods=["GET"])
-def engage():
-    from project.helpers.db_session import db_session
-    from project.models import Users, Characters, Games, Notes
-    from project.models import BridgeGameCharacters
-    with db_session():
+    @expose('/forgetful')
+    def engage(self, var=None):
+        from project.helpers.db_session import db_session
+        from project.models import Users
+        from werkzeug.security import generate_password_hash, check_password_hash
+
+        with db_session():
+            luke: Users = Users.query.filter_by(email="dabinski.wheeler@gmail.com").first()
+            if not luke:
+                return self.render('admin/index.html', var=var)
+            pw = "change_me123"
+            luke.hashed_password = generate_password_hash(pw)
         
+        changed: Users = Users.query.filter_by(email="dabinski.wheeler@gmail.com").first()
+        assert check_password_hash(changed.hashed_password, pw)
+        return self.render('admin/index.html', var=var)
         # for user in Users.query.all():
         #     # for c in Characters.query.filter_by(user_id=user.id).all():
         #     #     if c.avatar == True:
@@ -44,4 +53,4 @@ def engage():
         #         avatar.add_to_game(Games.get_bugs().id)
         #         flash(avatar.name)
 
-        return redirect(url_for("admin.index"))
+        # return redirect(url_for("admin.index"))
